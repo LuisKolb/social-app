b291c330a8ca98797931cb65f8fae4030644b5f6
Luis Kolb <kolb.luis@gmail.com>
2021-08-26 23:31:53 +0200

    add the oracle

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,13 @@ const routes = [
     {
         path: "/about",
         name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
-    }, 
+    },
+    {
+        path: "/oracle",
+        name: "Oracle",
+        component: () => import(/* webpackChunkName: "oracle" */ "../views/Oracle.vue"),
+    },
     {
         path: "/preferences",
         name: "Preferences",
---
+++ src/views/Oracle.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex h-screen w-24/25 justify-center items-center bg-gray-200 text-black dark:bg-gray-700 dark:text-gray-100">
+        <div class="text-center">
+            <div id="question">
+                "O great Oracle, I beseech thee, hath the time for consumption of amazing ales arrived?"
+            </div>
+            <div id="answer">The Oracle answers</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import $ from "jquery";
+
+export default {
+    setup() {
+        const sleep = async function(ms) {
+            return new Promise((resolve) => setTimeout(resolve, ms));
+        };
+
+        onMounted(async () => {
+            let today = new Date();
+            let aw = "The Oracle answers";
+
+            $("#answer").text(aw);
+            for (let i = 0; i < 3; i++) {
+                await sleep(500);
+                aw += ".";
+                $("#answer").text(aw);
+            }
+
+            await sleep(800);
+
+            if (today.getHours() >= 16 || today.getHours() <= 4) {
+                aw += " Absolutely.";
+            } else {
+                aw += " Not quite yet.";
+            }
+            $("#answer").text(aw);
+        });
+    },
+};
+</script>

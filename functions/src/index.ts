ad6bf8fa317b098bd6bc8eec6aa29bfa8f38d1a4
Luis Kolb <kolb.luis@gmail.com>
2021-08-28 00:31:56 +0200

    update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,14 +5,14 @@ admin.initializeApp(functions.config().firebase);
 
 exports.createUser = functions.auth.user().onCreate((user) => {
     const { uid, photoURL, displayName } = user;
-
     const userCollection = admin.firestore().collection('users');
-    userCollection.doc(uid).set({
+    
+    return userCollection.doc(uid).set({
         darkmode: false,
         photoURL: photoURL,
         displayName: displayName,
         createdAt: admin.firestore.FieldValue.serverTimestamp(),
         friends: [],
         uid: uid,
-    }).catch(() => 'obligatory catch');
+    })
 });
